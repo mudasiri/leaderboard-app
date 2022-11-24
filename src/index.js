96fdcd6fd127b2ffb0fbe7825a27b8f86769eca5
Mudasir Issah <danladi496@gmail.com>
2022-11-24 03:01:54 +0000

    created-add-game-request-and-getallscore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import './index.css';
-import displayScores from './modules/helper.js';
+import { addNewScore, getAllScores} from './modules/helper.js';
 
-window.onload(
-  displayScores(),
-);
+const scoreForm = document.getElementById('score-form');
+const refreshData = document.getElementById('refresh');
+
+scoreForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let user = document.getElementById('name');
+  let score = document.getElementById('score');
+
+  const newScore = {
+    user:user.value,
+    score:Number(score.value)
+  }
+  addNewScore(newScore);
+  user.value = '';
+  score.value = '';
+});
+
+refreshData.addEventListener('click', getAllScores);
+window.addEventListener('DOMContentLoaded' ,() => {
+  getAllScores();
+})
--- src/modules/helper.js
+++ src/modules/helper.js
@@ -1,29 +1,91 @@
-const scores = [
-  {
-    name: 'Mudasir Issah',
-    score: 5000,
-  },
-  {
-    name: 'Ali baba',
-    score: 500,
-  },
-  {
-    name: 'Ali Issah',
-    score: 600,
-  },
-  {
-    name: 'Mudasir Ali',
-    score: 5010,
-  },
-];
+const gameID = 'GKu1RIiZvbXu9pzTde2x';
+
 // grab ul tag displaying list items
 const scoresList = document.getElementById('scorelist');
 
 // create arrow function to loop through and display list items
-const displayScores = () => {
+export const displayScores = (scores) => {
+  scoresList.innerHTML = '';
   scores.forEach((score) => {
     // append li to ul tag
-    scoresList.insertAdjacentHTML('beforeend', `<li class='score-item'>${score.name}: ${score.score}</li>`);
+    scoresList.insertAdjacentHTML('beforeend', `<li class='score-item'>${score.user}: ${score.score}</li>`);
   });
 };
-export default displayScores;
+
+// show status of added form
+const showStatus = (message) => {
+  const statusDiv = document.getElementById('status');
+  if(message.includes('provide')){
+    statusDiv.className = 'error';
+  }
+  else {
+    statusDiv.className = 'success';
+  }
+  statusDiv.insertAdjacentHTML('beforeend',`<p>${message}</p>`);
+}
+
+export const addNewScore = async (newScore) => {
+  try {
+    const req = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newScore),
+    });
+    const data = await req.json();
+
+    if (!req.ok) {
+      showStatus(data.message);
+      return data;
+    }
+
+    getAllScores();
+    showStatus(data.result);
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getAllScores = async () => {
+  try {
+    const req = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`);
+    const data = await req.json();
+
+    if (!req.ok) {
+      return data;
+    }
+
+    const scores = data.result;
+    displayScores(scores);
+    return scores;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const createNewGame = async () => {
+  const gameName = {
+    name: 'Mudasir Issah Leaderboard Game',
+  };
+
+  try {
+    const req = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(gameName),
+    });
+
+    const result = await req.json();
+
+    if (!req.ok) {
+      return result;
+    }
+    return result;
+  } catch (error) {
+    return error;
+  }
+};
