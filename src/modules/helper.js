140ca39659081b9bdfc6a783bc39a5b0e14f74b1
Mudasir Issah <danladi496@gmail.com>
2022-11-24 01:03:47 +0000

    completed-structure-code

---
+++ src/modules/helper.js
@@ -0,0 +1,28 @@
+const scores = [
+    {
+        name: 'Mudasir Issah',
+        score: 5000
+    },
+    {
+        name: 'Ali baba',
+        score: 500
+    },
+    {
+        name: 'Ali Issah',
+        score: 600
+    },
+    {
+        name: 'Mudasir Ali',
+        score: 5010
+    }
+];
+// grab ul tag displaying list items
+const scoresList = document.getElementById('scorelist');
+
+// create arrow function to loop through and display list items
+export const displayScores = () => {
+    scores.forEach( (score)=>{
+        //append li to ul tag 
+        scoresList.insertAdjacentHTML("beforeend",`<li class='score-item'>${score.name}: ${score.score}</li>`);
+    })
+}
